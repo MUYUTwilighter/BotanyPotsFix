add1e9eb0468db3ee555c602c7326b50c96ffb5f
MUYU_Twilighter <1484605372@qq.com>
2024-10-03 22:58:14 -0400

    port to forge 1.18.2

--- src/main/java/cool/muyucloud/botanypotsfix/BotanyPotsFix.java
+++ src/main/java/cool/muyucloud/botanypotsfix/BotanyPotsFix.java
@@ -1,14 +1,14 @@
 package cool.muyucloud.botanypotsfix;
 
-import net.fabricmc.api.ModInitializer;
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
 
-import java.util.logging.Logger;
+@Mod("botanypotsfix")
+public class BotanyPotsFix {
+    private static final Logger LOGGER = LogUtils.getLogger();
 
-public class BotanyPotsFix implements ModInitializer {
-    public static final Logger LOGGER = Logger.getLogger("BotanyPotsFix");
-
-    @Override
-    public void onInitialize() {
+    public BotanyPotsFix() {
         LOGGER.info("Ready to optimize Botany Pots!");
         LOGGER.info("This mod is designed to fix issue #421 of Botany Pots, made by MUYU_Twilighter");
         LOGGER.info("The fix might be a part of Botany Pots in the future. At that time, this mod is no longer needed.");
